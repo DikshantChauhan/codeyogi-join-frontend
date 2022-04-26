010ce3b306f59942047270fab30dd191e42910d1
DikshantChauhan <chauhandikshant2734982@gmail.com>
2022-04-26 16:31:16 +0530

    error and redirection handling in main-exam screen

--- src/Pages/MainExamPage.tsx
+++ src/Pages/MainExamPage.tsx
@@ -1,6 +1,8 @@
 import { memo, FC, useState, useEffect, useCallback } from "react";
+import { Navigate } from "react-router";
 import { fetchExamQuestion, submitExamQuestion } from "../APIs/exam.api";
 import QuestionCard from "../Components/AdmissionTest/QuestionCard";
+import { ROUTE_HOMEPAGE } from "../constants.routes";
 import { answer, Question } from "../Models/Question";
 
 interface MainExamPageProps {}
@@ -9,6 +11,8 @@ const MainExamPage: FC<MainExamPageProps> = () => {
   const [selectedIteamHeight, setSelectedIteamHeight] = useState(null);
   const [isAnimating, setIsAnimating] = useState(false);
   const [isSubmiting, setIssubmitting] = useState(false);
+  const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState("");
   const [admissionQuestions, setAdmissionQuestions] = useState<Question[]>([]);
 
   const handleRef = useCallback((node) => {
@@ -30,40 +34,78 @@ const MainExamPage: FC<MainExamPageProps> = () => {
 
   const handleSubmit = (answer: answer) => {
     setIssubmitting(true);
+    setError("");
     submitExamQuestion(answer)
       .then(() => {
-        fetchExamQuestion().then((response) => {
-          if (!response) return;
-          setAdmissionQuestions([...admissionQuestions, response]);
-          handleSlide([...admissionQuestions, response]);
-        });
+        setIssubmitting(false);
+
+        setIsFetching(true);
+        setError("");
+        fetchExamQuestion()
+          .then((response) => {
+            if (!response) {
+              setAdmissionQuestions([]);
+            } else {
+              setAdmissionQuestions([...admissionQuestions, response]);
+              handleSlide([...admissionQuestions, response]);
+            }
+            setIsFetching(false);
+          })
+          .catch(() => {
+            setError("Unable to fetch question! please try again.");
+          });
       })
       .catch((error) => {
-        //handle answer not submitt
+        setIssubmitting(false);
+        setError("Unable to Submit your answer! please try again.");
         console.log(error);
       });
   };
 
   useEffect(() => {
-    fetchExamQuestion().then((response) => {
-      if (!response) return;
-      setAdmissionQuestions([...admissionQuestions, response]);
-    });
+    setIsFetching(true);
+    setError("");
+    fetchExamQuestion()
+      .then((response) => {
+        if (!response) {
+          setIsFetching(false);
+          setAdmissionQuestions([]);
+        } else {
+          setAdmissionQuestions([...admissionQuestions, response]);
+        }
+      })
+      .catch(() => {
+        setError("Unable to fetch question! please try again.");
+      });
   }, []);
+  // window.addEventListener("resize", () =>{
+  //   console.log("hello")
+  // })
 
   return (
     <div>
-      <div style={{ height: `${selectedIteamHeight}px` }} className="fixed transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        {admissionQuestions.map((question, index) => {
-          const ref = index === 0 ? handleRef : null;
-          const marginTop = index === 0 && isAnimating && `-${selectedIteamHeight}px`;
-          return (
-            <div ref={ref} key={index} style={{ marginTop: marginTop || "0px" }} className={`${isAnimating ? "transition-all duration-500" : ""}`}>
-              <QuestionCard isSubmitting={isSubmiting} trySubmit={handleSubmit} admissionQuestion={question} />
-            </div>
-          );
-        })}
-      </div>
+      {!isFetching && admissionQuestions.length === 0 ? (
+        <Navigate to={ROUTE_HOMEPAGE} />
+      ) : (
+        <div className="flex justify-center items-center min-h-screen">
+          <div style={{ height: `${selectedIteamHeight}px` }}>
+            {admissionQuestions.map((question, index) => {
+              const ref = index === 0 ? handleRef : null;
+              const marginTop = index === 0 && isAnimating && `-${selectedIteamHeight}px`;
+              return (
+                <div
+                  ref={ref}
+                  key={index}
+                  style={{ marginTop: marginTop || "0px" }}
+                  className={`${isAnimating ? "transition-all duration-500" : ""}`}
+                >
+                  <QuestionCard isSubmitting={isSubmiting} trySubmit={handleSubmit} admissionQuestion={question} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
